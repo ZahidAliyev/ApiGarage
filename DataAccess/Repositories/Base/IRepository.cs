dfcafd481c03e0dad1ed4aa1c33e6bfdb28aa9db
Zahid Aliyev <hermitaliyev96@gmail.com>
2023-12-07 16:05:35 +0400

    first commit

---
+++ DataAccess/Repositories/Base/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Base
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> GetAllAsync();
+        
+        Task<T> GetAsync(int id);
+        Task Create(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
